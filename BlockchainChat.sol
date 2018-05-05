35611618800975e7cbc2933ebdccb55e5ba3b0fd
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-05 11:32:35 +0530

    Update BlockchainChat.sol

--- BlockchainChat.sol
+++ BlockchainChat.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.0;
 contract BlockchainChat {
     
     mapping (address => string) public members;
-	mapping (address => uint) public timeCalled;
+    mapping (address => uint) public timeCalled;
 
     Message[] public messages;
     uint private messageNumber;
@@ -15,7 +15,7 @@ contract BlockchainChat {
     }    
     
     function newMessage(string _message) public {
-		// Check if the 5 seconds have been passed since the last function call.
+	// Check if the 5 seconds have been passed since the last function call.
         require(timeCalled[msg.sender] + 5 < now);
     	timeCalled[msg.sender] = now;
         messageNumber = messages.length++;
