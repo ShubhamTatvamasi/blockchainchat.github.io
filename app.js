b86ed09ce816c16ddf3e552ddac2cac6d7f12854
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-06 08:35:08 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 var blockchainChatContract;
 // Total Messages
 var totalMessages;
+
 // Web3 start function, It will run when the web page is loaded
 function startWeb3() {
 
   blockchainChatContract = web3.eth.contract(blockchainChatABI).at(blockchainChatAddress);
-
   getTotalMessages();
-  // getAllMessages();
 }
 
 // Get the list of total number of messages
 function getTotalMessages() {
+
   blockchainChatContract.totalMessages(function (error, result) {
     totalMessages = result.c[0];
     document.getElementById("TotalMessages").innerHTML = totalMessages;
@@ -20,19 +20,23 @@ function getTotalMessages() {
   })
 }
 
+// Get all Messages from smart contract
 function getAllMessages() {
+
     for (var i = 0; i < totalMessages; i++) {
       getMessages(i);
     }
 }
 
+// Get one message from smart contract
 function getMessages(messageNumber) {
+
   blockchainChatContract.messages(messageNumber, function (error, result) {
     getName(result);
   })
 }
 
-
+// Get name of the message sender from smart contract
 function getName(argument) {
 
     var newAddress = argument[0];
@@ -43,6 +47,7 @@ function getName(argument) {
     });
 }
 
+// Post the message on the website
 function postOnPage(_name, _address, _message, _time) {
 
         var d = new Date(0);
@@ -63,17 +68,16 @@ function postOnPage(_name, _address, _message, _time) {
         }
 }
 
+// Update new name in smart contract
 function newName() {
 
     var name = document.getElementById("NewName").value;
-
     blockchainChatContract.updateName(name, function (error, result) {})
-
 }
 
+// Send new message in smart contract
 function newMessage() {
 
     var message = document.getElementById("NewMessage").value;
-
     blockchainChatContract.newMessage(message, function (error, result) {})
 }
